ebfe3390c728cdeb1f8d325f257a211d1bf3b37b
kowsicavelusamy <36658808+kowsicavelusamy@users.noreply.github.com>
2018-02-20 14:25:21 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,7 @@
+a=int(input("entre the number:"))
+if(a<0):
+    print("the given number is negative")
+elif(a>0):
+    print("the given number is positive")
+else:
+    print("the given number is zero")
